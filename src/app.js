642f6270bcc117eb2cfaeb2befa6375b1af61283
sandeep bashyal <60920648+sandeep-bit@users.noreply.github.com>
2021-06-19 16:34:22 +0545

    setup app to heroku

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 3000;
 const hbs = require('hbs');
 const geocode = require('./utils/geocode');
 const forcast = require('./utils/forcast');
@@ -90,6 +91,6 @@ app.get('*', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('server is running');
+app.listen(port, () => {
+  console.log('server is running'+ port);
 });
